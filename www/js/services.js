ada6dff61ad38e1eef79fda4adfcd99b587a2c69
Cao Anh Quan <aquancva@gmail.com>
2016-02-11 18:36:12 +0700

    completed week 3 assignment

--- www/js/services.js
+++ www/js/services.js
@@ -37,10 +37,10 @@ angular.module('conFusion.services', ['ngResource'])
 
     }])
 
-    .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    .factory('favoriteFactory', ['$resource', 'baseURL', '$localStorage', function ($resource, baseURL, $localStorage) {
         var favFac = {};
-        var favorites = [];
-
+        var favorites = $localStorage.getObject('favorites', '[]');
+        console.log(favorites);
         favFac.addToFavorites = function (index) {
             for (var i = 0; i < favorites.length; i++) {
                 if (favorites[i].id == index) {
@@ -48,6 +48,7 @@ angular.module('conFusion.services', ['ngResource'])
                 }
             }
             favorites.push({id: index});
+            $localStorage.storeObject('favorites', favorites);
         };
 
         favFac.deleteFromFavorites = function (index) {
@@ -56,6 +57,7 @@ angular.module('conFusion.services', ['ngResource'])
                     favorites.splice(i, 1);
                 }
             }
+            $localStorage.storeObject('favorites', favorites);
         };
 
         favFac.getFavorites = function () {
